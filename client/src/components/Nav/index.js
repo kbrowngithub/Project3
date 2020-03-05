7eedc9b664679f71c51e3b27d8b7f4bdc2a80848
Richard Hamp <fozzie_77@hotmail.com>
2020-03-05 15:47:40 -0700

    navbar buttons styled

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,32 +1,45 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { AwesomeButton, AwesomeButtonProgress, AwesomeButtonSocial } from 'react-awesome-button';
+import 'react-awesome-button/dist/themes/theme-blue.css';
 
 function Nav() {
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link to="/" className="navbar-brand">ReactApp</Link>
-        <div className="">
-            <ul className="navbar-nav">
-            <li className="navbar-item">
-                    <Link to="/" className="nav-link">Home</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link to="/friends" className="nav-link">Friends</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link to="/recipes" className="nav-link">Recipes</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link to="/create" className="nav-link">Create User</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link to="/pantry" className="nav-link">Pantry</Link>
-                </li>
-            </ul>
-        </div>
-    </nav>
-);
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link to="/" className="navbar-brand">Chuck Wagon</Link>
+            <div className="">
+                <ul className="navbar-nav">
+                    <AwesomeButton
+                        type="secondary"
+                        size="small"
+                        class="margin"
+                    // action={(element, next) => doSomethingThenCall(next)}
+                    >Home</AwesomeButton>
+                       <AwesomeButtonProgress
+                        type="primary"
+                        size="small"
+                    // action={(element, next) => doSomethingThenCall(next)}
+                    >Friends</AwesomeButtonProgress>
+                     <AwesomeButtonProgress
+                        type="secondary"
+                        size="small"
+                    // action={(element, next) => doSomethingThenCall(next)}
+                    >Recipes</AwesomeButtonProgress>
+                      <AwesomeButtonProgress
+                        type="primary"
+                        size="small"
+                    // action={(element, next) => doSomethingThenCall(next)}
+                    >NewUser</AwesomeButtonProgress>
+                       <AwesomeButtonProgress
+                        type="secondary"
+                        size="small"
+                    // action={(element, next) => doSomethingThenCall(next)}
+                    >Pantry</AwesomeButtonProgress>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Nav;
