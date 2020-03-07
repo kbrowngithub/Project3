fd49633cec8fa9b6379a8b84fa57fcbcb8a598df
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2020-03-06 20:27:26 -0700

    Added Twilio package and sms send logic

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -23,5 +23,8 @@ export default {
     query = "bbq";
     // https://api.edamam.com/search?q=bbq&app_id=7e12516e&app_key=43686ac14fed6aac00402ab6b447842f
     console.log(axios.get(`https://api.edamam.com/search?q=${query}&app_id=${AppID}&app_key=${AppKey}`));
+  },
+  invite: function(smsData) {
+    return axios.post("/api/recipes/", smsData);
   }
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const recipeRoutes = require("./recipes");
 
-// Book routes
+// routes
 router.use("/recipes", recipeRoutes);
 
 module.exports = router;
