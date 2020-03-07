ab263687d2963c350e952505e51c2448910b6f96
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2020-03-06 20:27:26 -0700

    Added Twilio package and sms send logic

---
+++ client/src/pages/Invite.js
@@ -0,0 +1,112 @@
+import React, { Component } from "react";
+import { Input, TextArea, FormBtn } from "../components/Form";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+
+class Invite extends Component {
+  state = {
+    to: '',
+    from: '',
+    note: '',
+    message: {
+      to: '',
+      body: ''
+    },
+    submitting: false,
+    error: false
+  };
+
+  handleInputChange = event => {
+    // const { name, value } = event.target;
+    // this.setState({ [name]: value });
+    const name = event.target.getAttribute('name');
+    this.setState({
+      message: { ...this.state.message, [name]: event.target.value }
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.message.to) {
+      // alert(`Sending Invite to ${this.state.to}`);
+      console.log(`Sending sms to ${this.state.message.to}`);
+
+      this.setState({ submitting: true });
+      fetch('/api/recipes/messages', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.state.message)
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(`data.success = ${data.success}`);
+          alert(`Invite sent to ${this.state.message.to}`)
+          if (data.success) {
+            this.setState({
+              error: false,
+              submitting: false,
+              message: {
+                to: '',
+                body: ''
+              }
+            });
+          } else {
+            this.setState({
+              error: true,
+              submitting: false
+            });
+          }
+        });
+    } else {
+      alert(`Something went wrong!`)
+    }
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1>Who should I invite?</h1>
+            </Jumbotron>
+            <form>
+              <div className="form-group">
+                <label htmlFor="to">To:</label>
+                <input
+                  className="form-control"
+                  type="tel"
+                  name="to"
+                  id="to"
+                  value={this.state.message.to}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="body">Body:</label>
+                <textarea
+                  className="form-control" 
+                  rows="4"
+                  name="body"
+                  id="body"
+                  value={this.state.message.body}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <FormBtn
+                disabled={!(this.state.message.to)}
+                onClick={this.handleFormSubmit}
+              >
+                Send Invite
+              </FormBtn>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Invite;
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
-
+require("dotenv").config();
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+// Twilio Specific
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(pino);
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
