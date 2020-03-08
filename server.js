30ab76cfad5c8c5131a5bbe6e0031b831186f517
Turk <tuckerfiumara@gmail.com>
2020-03-08 12:41:16 -0700

    API route File Restructure

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./routes/api_routes");
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local').Strategy;
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,11 +17,41 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+routes(app);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/recipedb");
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds111940.mlab.com:11940/heroku_h7kb0lk7");
+
+// Express session
+app.use(
+  session({
+    secret: 'keyboardkitty',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport config
+// var User = require('./models/user');
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Start the API server
 app.listen(PORT, function() {
