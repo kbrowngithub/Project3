a13dede6dea77f4aba016424385bcb0414e3ef1b
Lucas Aho <aholucas11@gmail.com>
2020-03-10 12:33:27 -0600

    Created Table component for pantry page, now properly posts/gets ingredients

--- models/pantry.js
+++ models/pantry.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const pantrySchema = new Schema({
     name: { type: String, required: true },
-    quantity: { type: String }
+    quantity: { type: Number, required: true },
+    unit: {type: String}
 });
 
 const Pantry = mongoose.model("Pantry", pantrySchema);
