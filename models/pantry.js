f0c46faa81832cb7861e2bda259d8f6203208f8d
Lucas Aho <aholucas11@gmail.com>
2020-03-09 21:13:54 -0600

    added pantry model

---
+++ models/pantry.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const pantrySchema = new Schema({
+    name: { type: String, required: true },
+    quantity: { type: String }
+});
+
+const Pantry = mongoose.model("Pantry", pantrySchema);
+
+module.exports = Pantry; 
