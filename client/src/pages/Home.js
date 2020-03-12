5c1c1327ab03668f5e7da24753ac7b0837451299
Lucas Aho <aholucas11@gmail.com>
2020-03-12 14:21:15 -0600

    Drinks are now properly found with drink button, however 'vodka' is hardcoded until an input method is decided on

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+import Grid, { Col } from "../components/Grid"
 import { Button, Card } from 'react-bootstrap';
 import RecipeSearch from "../components/RecipeSearch";
+import DrinkSearch from "../components/DrinkSearch";
 import RecipeCard from "../components/RecipeCard";
+import DrinkCard from "../components/DrinkCard";
 
 class Home extends Component {
     constructor(props) {
         super(props) 
         
         this.state = {
-            recipeData: []
+            recipeData: [],
+            drinkData: []
         }
         this.updateRecipes = this.updateRecipes
+        this.updateDrinks = this.updateDrinks
         
     }
 
     updateRecipes = (array) => {
         this.setState({ recipeData: array });
-        console.log(this.state.recipeData)
+
+    }
+
+    updateDrinks = (array) => {
+        this.setState({ drinkData: array });
+        console.log(this.state.drinkData)
     }
 
     render() {
@@ -24,30 +34,48 @@ class Home extends Component {
             <div>
                 <h3 className="content-center">Home</h3>
                 <div className="row">
-                    <Button variant="info" style={{ margin: '10px' }}>Drink Recipes</Button>
+                    <DrinkSearch updateDrinksCB={ this.updateDrinks }></DrinkSearch>
 
                     <RecipeSearch updateRecipesCB={ this.updateRecipes }></RecipeSearch>
                 </div>
                 <h4>Found Recipes</h4>
 
                 <div className="row">
-                    {this.state.recipeData.length ? (
-                        this.state.recipeData.map(recipe => (
-                            <RecipeCard
-                            id={recipe.id}
-                            key={recipe.id}
-                            image={recipe.image}
-                            summary={recipe.summary}
-                            title={recipe.title}
-                            missingIngredients={recipe.missedIngredients}
-                            usedIngredients={recipe.usedIngredients}
-                            />
-                        ))
-                        
-                    ) : (
-                        <h3>Click the Recipe Buttons to Display New Recipes</h3>
-                    )}
+                    <Col size="md-6">
+                        {this.state.recipeData.length ? (
+                            this.state.recipeData.map(recipe => (
+                                <RecipeCard
+                                id={recipe.id}
+                                key={recipe.id}
+                                image={recipe.image}
+                                summary={recipe.summary}
+                                title={recipe.title}
+                                missingIngredients={recipe.missedIngredients}
+                                usedIngredients={recipe.usedIngredients}
+                                />
+                            ))
+                            
+                        ) : (
+                            <h3>Click the Recipe Buttons to Display New Recipes</h3>
+                        )}
+                    </Col>
+                
 
+                    <Col size="md-6">
+                        {this.state.drinkData.length ? (
+                            this.state.drinkData.map(drink => (
+                                <DrinkCard
+                                id={drink.idDrink}
+                                key={drink.idDrink}
+                                title={drink.strDrink}
+                                image={drink.strDrinkThumb}
+                                />
+                            ))
+                            
+                        ) : (
+                            <h3>Click the Drink Buttons to Display New Drinks</h3>
+                        )}
+                    </Col>
                 </div>
             </div>
         )
