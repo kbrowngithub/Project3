a0ed8b2f20cf1d0c02d3a38019a98c61e53595a6
Lucas Aho <aholucas11@gmail.com>
2020-03-11 18:59:16 -0600

    Recipe cards now load images and titles

--- client/src/components/RecipeCard/index.js
+++ client/src/components/RecipeCard/index.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
 function RecipeCard(props) {
-    console.log(props.title);
     return (
-        <p>props.title</p>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={props.image} />
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
     )
 }
 
