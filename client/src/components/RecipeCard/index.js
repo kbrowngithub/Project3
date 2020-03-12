e60a66ef05594c688fec0c546c69d9445f635628
Lucas Aho <aholucas11@gmail.com>
2020-03-11 18:53:17 -0600

    Recipe data is now sent as props to the recipe card component

---
+++ client/src/components/RecipeCard/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Button, Card } from 'react-bootstrap';
+
+function RecipeCard(props) {
+    console.log(props.title);
+    return (
+        <p>props.title</p>
+    )
+}
+
+export default RecipeCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,90 +1,54 @@
 import React, { Component } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import RecipeSearch from "../components/RecipeSearch";
+import RecipeCard from "../components/RecipeCard";
 
-export default function Home() {
+class Home extends Component {
+    constructor(props) {
+        super(props) 
+        
+        this.state = {
+            recipeData: []
+        }
+        this.updateRecipes = this.updateRecipes
+        
+    }
 
-    return (
-        <div>
-            <h3 className="content-center">Home</h3>
-            <div className="row">
-                <Button variant="info" style={{ margin: '10px' }}>Drink Recipes</Button>
+    updateRecipes = array => {
+        this.setState({ recipeData: array });
+    }
 
-                <RecipeSearch></RecipeSearch>
+    render() {
+        return (
+            <div>
+                <h3 className="content-center">Home</h3>
+                <div className="row">
+                    <Button variant="info" style={{ margin: '10px' }}>Drink Recipes</Button>
+
+                    <RecipeSearch updateRecipesCB={ this.updateRecipes }></RecipeSearch>
+                </div>
+                <h4>Found Recipes</h4>
+
+                <div className="row">
+                    {this.state.recipeData.length ? (
+                        this.state.recipeData.map(recipe => (
+                            <RecipeCard
+                            id={recipe.id}
+                            key={recipe.id}
+                            image={recipe.image}
+                            title={recipe.title}
+                            missingIngredients={recipe.missedIngredients}
+                            usedIngredients={recipe.usedIngredients}
+                            />
+                        ))
+                        
+                    ) : (
+                        <h3>Click the Recipe Buttons to Display New Recipes</h3>
+                    )}
+
+                </div>
             </div>
-            <h4>Found Recipes</h4>
-
-            <div className="row">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-    </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-    </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-    </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </div>
-            <br></br>
-            <div className="row">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-    </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-    </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                    </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </div>
-        </div>
-    )
+        )
+    }
 }
+export default Home;
