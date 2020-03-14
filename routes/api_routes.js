a95b5a0dcefe1ed3248a52f26648700bebed51b2
Lucas Aho <aholucas11@gmail.com>
2020-03-14 11:12:57 -0600

    Readability changes to mongodb and ingredients added to recipe slides

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -76,7 +76,7 @@ function api_routes(app) {
     app.delete('/api/pantry/:id', pantryController.remove);
 
     app.post('/api/spoon', function (req, res) {
-        var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + process.env.foodAPIKey + "&ingredients=" + req.body.query + "&ranking=2&number=10&ignorePantry=true";
+        var queryURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + process.env.foodAPIKey + "&ingredients=" + req.body.query + "&limitLicense=true&ranking=2&number=3&ignorePantry=true";
         axios.get(queryURL)
             .then(response => {
                 var recipeSumms = [];
