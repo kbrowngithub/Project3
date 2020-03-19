c3ea1cf924b80ffff4d766c897112046b6ead9de
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2020-03-18 20:19:10 -0600

    Drink and Food returns on main page are rowed and scrollable

--- client/src/components/RecipeCard/index.js
+++ client/src/components/RecipeCard/index.js
@@ -7,28 +7,27 @@ function RecipeCard(props) {
     var shortText = props.summary.substr(0, 200);
     var cleanText = shortText.replace(/<\/?[^>]+(>|$)/g, "") + "...";
     return (
+    
         <Card key={props.key} className ="cardFull">
             <Card.Img variant="top" src={props.image} />
             <Card.Body>
                 <Card.Title className="cardTitle">{props.title}</Card.Title>
-                <Card.Text>
+
+                {/* <Card.Text>
                     {cleanText}
-                </Card.Text>
-<<<<<<< HEAD
-                <Link className="cardButton" to={{
-                    pathname:'/Recipes',
-=======
-                <Link variant="primary" to={{
+                </Card.Text> */}
+                
+                <p className="missing">Missing {props.missingIngredients.length} Ingredient(s)</p>
+                <Link className="cardButton"
+                variant="primary" to={{
                     pathname:'/recipes/new/' + props.id,
->>>>>>> f64053299bfa6278c60393bf9ac491501262c94e
                     state: {
                         recipeData: props
                     }
                 }}>Check It Out</Link> 
-                <p>Missing {props.missingIngredients.length} Ingredient(s)</p>
-                
             </Card.Body>
         </Card>
+     
     )
 }
 
