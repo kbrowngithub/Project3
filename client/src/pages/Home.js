3206b09b521a5e972a47d50c81c357984ebd6783
Lucas Aho <aholucas11@gmail.com>
2020-03-23 20:19:08 -0600

    wip on recipe saves, fix for searches

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -71,8 +71,7 @@ class Home extends Component {
                                             image={drink.strDrinkThumb}
                                         />
                                     ))
-                                ) :
-                                    (
+                                ) : (
                                         <h3></h3>
                                     )}
                             </div>
--- controllers/pantryController.js
+++ controllers/pantryController.js
@@ -1,69 +1,55 @@
 const db = require("../models");
 
 module.exports = {
-    findAll: function(req, res) {
-      db.Pantry
-          .find(req.query)
-          .sort({ date: -1 })
-          .then(dbModel => res.json(dbModel))
-          .catch(err => res.status(422).json(err));
-    },
-    findById: function(req, res) {
-        db.Pantry
-          .findById(req.params.id)
-          .then(dbModel => res.json(dbModel))
-          .catch(err => res.status(422).json(err));
-    },
-    create: function(req,res) { 
-      db.Pantry.findOne({ userEmail: req.body.userEmail }, function(err, user) {
-        if(err) {console.log(err)}
-        if (user) {
-          console.log("User found")
-
-          var newIngredient = {
-            name: req.body.ingredients.name,
-            quantity: req.body.ingredients.quantity,
-            unit: req.body.ingredients.unit
-          }
-          
-          console.log(newIngredient)
-          console.log(req.body.userEmail )
-
-          db.Pantry.findOneAndUpdate(
-            { userEmail: req.body.userEmail },
-            { $push : { ingredients: newIngredient }}
-          )
-          .then(dbModel => {res.json(dbModel)
-          console.log(dbModel)})
-          .catch(err => res.status(422).json(err))
-        } else {
-          db.Pantry
-            .create(req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-            console.log("Getting user email: " + req.body.userEmail)
+  findAll: function (req, res) {
+    db.Pantry
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Pantry
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.Pantry.findOne({ userEmail: req.body.userEmail }, function (err, user) {
+      if (err) { console.log(err) }
+      if (user) {
+        var newIngredient = {
+          name: req.body.ingredients.name,
+          quantity: req.body.ingredients.quantity,
+          unit: req.body.ingredients.unit
         }
-      })
-
-    },
-    update: function(req, res) {
-      console.log("updateemail: ",req.body.email)  
-      console.log("id: ",req.params.id)
-      console.log(req.body.quantity)
-      db.Pantry
-          .findOneAndUpdate({ userEmail: req.body.email , "ingredients._id": req.params.id },
-          { $set : {"ingredients.$.quantity": req.body.quantity }})
-          .then(dbModel => 
-            res.json(dbModel))
-          .catch(err => res.status(422).json(err));
-      },
-    remove: function(req, res) {
-      console.log("Req params: ",req.params.id);
-      console.log("email",req.params.email);
+        db.Pantry.findOneAndUpdate(
+          { userEmail: req.body.userEmail },
+          { $push: { ingredients: newIngredient } }
+        )
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err))
+      } else {
         db.Pantry
-          .update({ userEmail: req.params.email },
-            {$pull: {ingredients: { _id: req.params.id}}})
+          .create(req.body)
           .then(dbModel => res.json(dbModel))
           .catch(err => res.status(422).json(err));
-    }
+      }
+    })
+  },
+  update: function (req, res) {
+    db.Pantry
+      .findOneAndUpdate({ userEmail: req.body.email, "ingredients._id": req.params.id },
+        { $set: { "ingredients.$.quantity": req.body.quantity } })
+      .then(dbModel =>
+        res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    db.Pantry
+      .update({ userEmail: req.params.email },
+        { $pull: { ingredients: { _id: req.params.id } } })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
 } 
