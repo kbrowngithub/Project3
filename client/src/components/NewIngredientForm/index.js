675ad273f9d6579b09c802e9be857292aa85b19c
Lucas Aho <aholucas11@gmail.com>
2020-03-25 16:06:26 -0600

    fixed pantry forms

--- client/src/components/NewIngredientForm/index.js
+++ client/src/components/NewIngredientForm/index.js
@@ -3,18 +3,18 @@ import { useForm } from 'react-hook-form'
 
 
 export function IngredientForm(props) {
-    const { register, handleSubmit, errors } = useForm();
-    const onSubmit = data => {
+    const { register, handleSubmit, errors } = useForm({mode: 'onChange'});
+    const onSubmit = (data, e) => {
+        e.target.reset();
         props.sendIngredient(data);
     };
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <input
+                defaultValue=""
                 name="newIngredient"
                 type="text"
                 placeholder="Add Ingredient(Required)"
-                value={props.newIngredient}
-                onChange={props.addIngredient}
                 ref={register({
                     required: true,
                     pattern: /^[a-zA-Z0-9_.-/,%]+( [a-zA-Z0-9_.-/,%]+)*$/,
@@ -27,11 +27,10 @@ export function IngredientForm(props) {
             {errors.newIngredient && errors.newIngredient.type === "minLength" && "Ingredient name must be longer"}
             {errors.newIngredient && errors.newIngredient.type === "maxLength" && "Ingredient name must be shorter"}
             <input
+                defaultValue=""
                 name="newQuantity"
                 type="text"
                 placeholder="Add Quantity(Required)"
-                value={props.newQuantity}
-                onChange={props.addIngredient}
                 ref={register({
                     required: true,
                     pattern: /^[0-9]+$/,
@@ -43,11 +42,10 @@ export function IngredientForm(props) {
             {errors.newQuantity && errors.newQuantity.type === "min" && "Quantity must be greater than 0"}
             {errors.newQuantity && errors.newQuantity.type === "max" && "Quantity must less than 100"}
             <input
+                defaultValue=""
                 name="newUnit"
                 type="text"
                 placeholder="Add Unit"
-                defaultValue={props.newUnit}
-                onChange={props.addIngredient}
                 ref={register({
                     required: false,
                     maxLength: 10
@@ -67,11 +65,10 @@ export function DrinkForm(props) {
         <div className="fontNorm">
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input
+                    defaultValue=""
                     name="newDrink"
                     type="text"
                     placeholder="Add Liquor(Required)"
-                    value={props.newDrink}
-                    onChange={props.addDrink}
                     ref={register({
                         required: true,
                         pattern: /^[a-zA-Z0-9]*$/,
